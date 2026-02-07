225ad3bdbc483e547aaa2b630bf5548b5f9dc3c8
songyunbae <baesongyun@gmail.com>
2026-02-07 15:25:38 +0900

    BOJ 단어뒤집기2

--- 04_Algorithm/BOJ/17413_단어뒤집기2/s1.py
+++ 04_Algorithm/BOJ/17413_단어뒤집기2/s1.py
@@ -1,51 +1,53 @@
 import sys
 sys.stdin = open('input.txt')
 
-# <problem>17413<is hardest>problem ever<end>
-# <problem>31471<is hardest>melborp reve<end> 
+def reverse_word (s_idx, e_idx, answer, org_str):
+    # 마지막 인덱스 대입.
+    if (i == len(str_list)-1): e_idx = i
+    
+    tmp_str = org_str[s_idx:e_idx+1]
+    answer += tmp_str[::-1]
+   
+    if(s == ' '): answer+=s      
+    s_idx, e_idx = -1, -1
+    return s_idx, e_idx, answer
 
-# 괄호 는 그대로 두고 
 
 str_list = str(input())
+
 answer = ''
-s_idx, e_idx = 0, 0
+flag = False # 인덱스 만남 여부
+s_idx, e_idx = -1, -1
 
 for i, s in enumerate(str_list):
-    print('현재 글자', s)
-    # 괄호가 오면, 일단 넘어간다
+    # 여는 괄호
     if (s == '<'):
+        # 바꿀 단어가 있다면 함수 호출
+        if (s_idx != -1): s_idx, e_idx, answer = reverse_word(s_idx, e_idx, answer, str_list)
+        flag = True
         answer += s
-        pass
+        continue
+    # 닫는 괄호
     elif (s == '>'):
-        pass
-    
-    # 공백을 만나면, 그 인덱스 그대로 넣을거다.
-    # 기록한 인덱스는 초기화 한다.
-    elif (s == ' '):
+        flag = False
         answer += s
-        gap = e_idx - s_idx
-        for j in range(0, gap+1):
-            new_idx = (s_idx + gap + j) - (2*j)
-        
-            answer += str_list[new_idx]
-        
-        s_idx, e_idx = 0, 0
-        pass
-    # 괄호가 아니면 인덱스 바로 메모
+        continue
     
-    if (s_idx == 0): 
+    # 괄호 안의 문자인 경우.
+    if (flag):
+        answer += s
+        continue
+    
+    # 공백을 만나거나, 문자열의 마지막인 경우
+    elif (s == ' ' or (i == len(str_list)-1)):
+        s_idx, e_idx, answer = reverse_word(s_idx, e_idx, answer, str_list)
+        continue
+    
+    # 바꿔야 하는 대상 문자열의 마지막 인덱스.
+    e_idx = i
+    
+    if (s_idx == -1): 
         s_idx = i
-        pass
-
-    if (e_idx == 0):
-        e_idx = i
-        pass
+        continue    
     
 print(answer)
-    # 그 다음부터 공백이 아니라면 인덱스를 메모한다.
-    # 공백을 만나면, -1 해서 그 인덱스 까지 메모 후 다시 반복문돌려서 문자열 붙여넣기
-
-    
-# 변경 대상 시작, 끝 찾기
-
-# 시작 끝 -2 씩해서 자리 교체 해서 담기
